9a8fde1a65fae6ab215f9b91093cb3d72e730065
Eugene Nosko <coder@venux.com>
2016-10-04 08:42:15 -0400

    Bug fixes

--- src/webidl.js
+++ src/webidl.js
@@ -3,6 +3,10 @@
   Process: API generation
 */
 
-{
-  // Generated code goes here
-}
+Object.defineProperty(typeof global === "object" ? global : window, "PI", {
+    value:        3.141593,
+    enumerable:   true,
+    writable:     false,
+    configurable: false
+})
+PI > 3.0;
