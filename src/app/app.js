1bcb20a1f8dad1fb58b83b37a6c854e2761b3010
Eugene Nosko <coder@venux.com>
2018-01-02 07:35:16 -0500

    Application

--- src/app/app.js
+++ src/app/app.js
@@ -1,108 +1,160 @@
-var Stream = require('stream')
+'use strict'
 
-// through
-//
-// a stream that does nothing but re-emit the input.
-// useful for aggregating a series of changing but not ending streams into one stream)
+const iterations = +process.env.BENCH_TEST_ITERATION || 100
+const testCount = +process.env.BENCH_TEST_COUNT || 20
 
-exports = module.exports = through
-through.through = through
+const tests = [
+  'baseline',
+  'minipass',
+  'extend-minipass',
+  'through2',
+  'extend-through2',
+  'passthrough',
+  'extend-transform'
+]
 
-//create a readable writable stream.
+const manyOpts = [ 'many', 'single' ]
+const typeOpts = [ 'buffer', 'string', 'object' ]
 
-function through (write, end, opts) {
-  write = write || function (data) { this.queue(data) }
-  end = end || function () { this.queue(null) }
+const main = () => {
+  const spawn = require('child_process').spawn
+  const node = process.execPath
 
-  var ended = false, destroyed = false, buffer = [], _ended = false
-  var stream = new Stream()
-  stream.readable = stream.writable = true
-  stream.paused = false
+  const results = {}
 
-//  stream.autoPause   = !(opts && opts.autoPause   === false)
-  stream.autoDestroy = !(opts && opts.autoDestroy === false)
+  const testSet = []
+  tests.forEach(t =>
+    manyOpts.forEach(many =>
+      typeOpts.forEach(type =>
+        new Array(testCount).join(',').split(',').forEach(() =>
+        t !== 'baseline' || (many === 'single' && type === 'object')
+          ? testSet.push([t, many, type]) : null))))
 
-  stream.write = function (data) {
-    write.call(this, data)
-    return !stream.paused
-  }
+  let didFirst = false
+  const mainRunTest = t => {
+    if (!t)
+      return afterMain(results)
 
-  function drain() {
-    while(buffer.length && !stream.paused) {
-      var data = buffer.shift()
-      if(null === data)
-        return stream.emit('end')
+    const k = t.join('\t')
+    if (!results[k]) {
+      results[k] = []
+      if (!didFirst)
+        didFirst = true
       else
-        stream.emit('data', data)
+        process.stderr.write('\n')
+
+      process.stderr.write(k + ' #')
+    } else {
+      process.stderr.write('#')
     }
+
+    const c = spawn(node, [__filename].concat(t), {
+      stdio: [ 'ignore', 'pipe', 2 ]
+    })
+    let out = ''
+    c.stdout.on('data', c => out += c)
+    c.on('close', (code, signal) => {
+      if (code || signal)
+        throw new Error('failed: ' + code + ' ' + signal)
+      results[k].push(+out)
+      mainRunTest(testSet.shift())
+    })
   }
 
-  stream.queue = stream.push = function (data) {
-//    console.error(ended)
-    if(_ended) return stream
-    if(data === null) _ended = true
-    buffer.push(data)
-    drain()
-    return stream
-  }
-
-  //this will be registered as the first 'end' listener
-  //must call destroy next tick, to make sure we're after any
-  //stream piped from here.
-  //this is only a problem if end is not emitted synchronously.
-  //a nicer way to do this is to make sure this is the last listener for 'end'
-
-  stream.on('end', function () {
-    stream.readable = false
-    if(!stream.writable && stream.autoDestroy)
-      process.nextTick(function () {
-        stream.destroy()
-      })
-  })
-
-  function _end () {
-    stream.writable = false
-    end.call(stream)
-    if(!stream.readable && stream.autoDestroy)
-      stream.destroy()
-  }
-
-  stream.end = function (data) {
-    if(ended) return
-    ended = true
-    if(arguments.length) stream.write(data)
-    _end() // will emit or queue
-    return stream
-  }
-
-  stream.destroy = function () {
-    if(destroyed) return
-    destroyed = true
-    ended = true
-    buffer.length = 0
-    stream.writable = stream.readable = false
-    stream.emit('close')
-    return stream
-  }
-
-  stream.pause = function () {
-    if(stream.paused) return
-    stream.paused = true
-    return stream
-  }
-
-  stream.resume = function () {
-    if(stream.paused) {
-      stream.paused = false
-      stream.emit('resume')
-    }
-    drain()
-    //may have become paused again,
-    //as drain emits 'data'.
-    if(!stream.paused)
-      stream.emit('drain')
-    return stream
-  }
-  return stream
+  mainRunTest(testSet.shift())
 }
 
+const afterMain = results => {
+  console.log('test\tmany\ttype\tops/s\tmean\tmedian\tmax\tmin' +
+              '\tstdev\trange\traw')
+  // get the mean, median, stddev, and range of each test
+  Object.keys(results).forEach(test => {
+    const k = results[test].sort((a, b) => a - b)
+    const min = k[0]
+    const max = k[ k.length - 1 ]
+    const range = max - min
+    const sum = k.reduce((a,b) => a + b, 0)
+    const mean = sum / k.length
+    const ops = iterations / mean * 1000
+    const devs = k.map(n => n - mean).map(n => n * n)
+    const avgdev = devs.reduce((a,b) => a + b, 0) / k.length
+    const stdev = Math.pow(avgdev, 0.5)
+    const median = k.length % 2 ? k[Math.floor(k.length / 2)] :
+      (k[k.length/2] + k[k.length/2+1])/2
+    console.log(
+      '%s\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%s', test, round(ops),
+      round(mean), round(median),
+      max, min, round(stdev), round(range),
+      k.join('\t'))
+  })
+}
+
+const round = num => Math.round(num * 1000)/1000
+
+const test = (testname, many, type) => {
+  const timer = require('./lib/timer.js')
+  const Class = getClass(testname)
+
+  const done = timer()
+  runTest(Class, many, type, iterations, done)
+}
+
+// don't blow up the stack! loop unless deferred
+const runTest = (Class, many, type, iterations, done) => {
+  const Nullsink = require('./lib/nullsink.js')
+  const Numbers = require('./lib/numbers.js')
+  const opt = {}
+  if (type === 'string')
+    opt.encoding = 'utf8'
+  else if (type === 'object')
+    opt.objectMode = true
+
+  while (iterations--) {
+    let finished = false
+    let inloop = true
+    const after = iterations === 0 ? done
+      : () => {
+        if (iterations === 0)
+          done()
+        else if (inloop)
+          finished = true
+        else
+          runTest(Class, many, type, iterations, done)
+      }
+
+    const out = new Nullsink().on('finish', after)
+    let sink = Class ? new Class(opt) : out
+
+    if (many && Class)
+      sink = sink
+        .pipe(new Class(opt))
+        .pipe(new Class(opt))
+        .pipe(new Class(opt))
+        .pipe(new Class(opt))
+
+    if (sink !== out)
+      sink.pipe(out)
+
+    new Numbers(opt).pipe(sink)
+
+    // keep tight-looping if the stream is done already
+    if (!finished) {
+      inloop = false
+      break
+    }
+  }
+}
+
+const getClass = testname =>
+  testname === 'through2' ? require('through2').obj
+  : testname === 'extend-through2' ? require('./lib/extend-through2.js')
+  : testname === 'minipass' ? require('../')
+  : testname === 'extend-minipass' ? require('./lib/extend-minipass.js')
+  : testname === 'passthrough' ? require('stream').PassThrough
+  : testname === 'extend-transform' ? require('./lib/extend-transform.js')
+  : null
+
+if (!process.argv[2])
+  main()
+else
+  test(process.argv[2], process.argv[3] === 'many', process.argv[4])
