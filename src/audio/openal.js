4e5b8e6cfc4b378a57d4bacff3996febfb048c26
Eugene Nosko <coder@venux.com>
2018-01-02 07:36:37 -0500

    OpenAL

--- src/audio/openal.js
+++ src/audio/openal.js
@@ -1,45 +1,38 @@
-var test = require('tape')
-var through = require('../')
+'use strict';
 
-// must emit end before close.
+const url = require('url');
+const https = require('https');
 
-test('end before close', function (assert) {
-  var ts = through()
-  var ended = false, closed = false
+/**
+ * This currently needs to be applied to all Node.js versions
+ * in order to determine if the `req` is an HTTP or HTTPS request.
+ *
+ * There is currently no PR attempting to move this property upstream.
+ */
+https.request = (function(request) {
+  return function(_options, cb) {
+    let options;
+    if (typeof _options === 'string') {
+      options = url.parse(_options);
+    } else {
+      options = Object.assign({}, _options);
+    }
+    if (null == options.port) {
+      options.port = 443;
+    }
+    options.secureEndpoint = true;
+    return request.call(https, options, cb);
+  };
+})(https.request);
 
-  ts.on('end', function () {
-    assert.ok(!closed)
-    ended = true
-  })
-  ts.on('close', function () {
-    assert.ok(ended)
-    closed = true
-  })
-
-  ts.write(1)
-  ts.write(2)
-  ts.write(3)
-  ts.end()
-  assert.ok(ended)
-  assert.ok(closed)
-  assert.end()
-})
-
-test('end only once', function (t) {
-
-  var ts = through()
-  var ended = false, closed = false
-
-  ts.on('end', function () {
-    t.equal(ended, false)
-    ended = true
-  })
-
-  ts.queue(null)
-  ts.queue(null)
-  ts.queue(null)
-
-  ts.resume()
-
-  t.end()
-})
+/**
+ * This is needed for Node.js >= 9.0.0 to make sure `https.get()` uses the
+ * patched `https.request()`.
+ *
+ * Ref: https://github.com/nodejs/node/commit/5118f31
+ */
+https.get = function(options, cb) {
+  const req = https.request(options, cb);
+  req.end();
+  return req;
+};
