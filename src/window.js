60a25bfd50b9b8c700e7c77a7a4e679cd4ec6d6c
Eugene Nosko <coder@venux.com>
2018-01-02 07:40:08 -0500

    Window

--- src/window.js
+++ src/window.js
@@ -1,36 +1,44 @@
-var fs = require ('fs');
-var net = require('net');
-var join = require('path').join;
-var file = join(__dirname, 'fixtures','all_npm.json');
-var it = require('it-is');
-var JSONStream = require('../');
+'use strict'
 
-var str = fs.readFileSync(file);
+const ls = require('../ls.js')
+const get = require('../get.js')
+const put = require('../put.js')
+const rm = require('../rm.js')
+const verify = require('../verify.js')
+const setLocale = require('../lib/util/y.js').setLocale
+const clearMemoized = require('../lib/memoization.js').clearMemoized
+const tmp = require('../lib/util/tmp.js')
 
-var datas = {}
+setLocale('en')
 
-var server = net.createServer(function(client) {
-    var data_calls = 0;
-    var parser = JSONStream.parse(['rows', true, 'key']);
-    parser.on('data', function(data) {
-        ++ data_calls;
-        datas[data] = (datas[data] || 0) + 1
-        it(data).typeof('string')
-    });
+const x = module.exports
 
-    parser.on('end', function() {
-        console.log('END')
-        var min = Infinity
-        for (var d in datas)
-          min = min > datas[d] ? datas[d] : min
-        it(min).equal(3);
-        server.close();
-    });
-    client.pipe(parser);
-});
-server.listen(9999);
+x.ls = cache => ls(cache)
+x.ls.stream = cache => ls.stream(cache)
 
-var client = net.connect({ port : 9999 }, function() {
-    var msgs = str + ' ' + str + '\n\n' + str
-    client.end(msgs);
-});
+x.get = (cache, key, opts) => get(cache, key, opts)
+x.get.byDigest = (cache, hash, opts) => get.byDigest(cache, hash, opts)
+x.get.stream = (cache, key, opts) => get.stream(cache, key, opts)
+x.get.stream.byDigest = (cache, hash, opts) => get.stream.byDigest(cache, hash, opts)
+x.get.copy = (cache, key, dest, opts) => get.copy(cache, key, dest, opts)
+x.get.copy.byDigest = (cache, hash, dest, opts) => get.copy.byDigest(cache, hash, dest, opts)
+x.get.info = (cache, key) => get.info(cache, key)
+x.get.hasContent = (cache, hash) => get.hasContent(cache, hash)
+
+x.put = (cache, key, data, opts) => put(cache, key, data, opts)
+x.put.stream = (cache, key, opts) => put.stream(cache, key, opts)
+
+x.rm = (cache, key) => rm.entry(cache, key)
+x.rm.all = cache => rm.all(cache)
+x.rm.entry = x.rm
+x.rm.content = (cache, hash) => rm.content(cache, hash)
+
+x.setLocale = lang => setLocale(lang)
+x.clearMemoized = () => clearMemoized()
+
+x.tmp = {}
+x.tmp.mkdir = (cache, opts) => tmp.mkdir(cache, opts)
+x.tmp.withTmp = (cache, opts, cb) => tmp.withTmp(cache, opts, cb)
+
+x.verify = (cache, opts) => verify(cache, opts)
+x.verify.lastRun = cache => verify.lastRun(cache)
